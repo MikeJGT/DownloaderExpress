5193c0fe51f559db6a0985098dcae9fcccdb7d51
miky3000 <mikecode808@gmail.com>
2023-10-10 18:37:14 +0200

    upload cors

--- routes/api/download.js
+++ routes/api/download.js
@@ -6,19 +6,28 @@ const ytdl = require('ytdl-core');
 router.get('/:id/:itag', async (req, res) => {
     try {
         const url = `https://www.youtube.com/watch?v=${req.params.id}`;
-        res.writeHead(200, {
-            'Content-Type': 'text/plain; charset=utf-8',
-            'Transfer-Encoding': 'chunked',
-            'X-Content-Type-Options': 'nosniff',
-            'Access-Control-Allow-Origin': '*'
-        })
-        ytdl(url, { quality: `${req.params.itag}`, filter: format => format.container === 'mp4' }).on('data', (chunk) => {
+        // res.writeHead(200, {
+        //     'Content-Type': 'text/plain; charset=utf-8',
+        //     'Transfer-Encoding': 'chunked',
+        //     'X-Content-Type-Options': 'nosniff',
+        //     'Access-Control-Allow-Origin': '*'
+        // })
+        ytdl(url, {
+            requestOptions: {
+                'Content-Type': 'text/plain; charset=utf-8',
+                'Transfer-Encoding': 'chunked',
+                'X-Content-Type-Options': 'nosniff',
+                'Access-Control-Allow-Origin': '*'
+            }
+        }, { quality: `${req.params.itag}`, filter: format => format.container === 'mp4' }).on('data', (chunk) => {
             res.write(chunk);
 
             //console.log('chunk', chunk)
         }).on('end', () => {
+
             res.end();
         })
+        console.log('YT', yt);
 
     } catch (err) {
         res.json({ fatal: err.message })
