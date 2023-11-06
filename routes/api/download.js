0640f8117fedb63589e687ee20b78f51a732c66c
MikeJGT <117930380+MikeJGT@users.noreply.github.com>
2023-11-06 21:20:38 +0100

    Update download.js

--- routes/api/download.js
+++ routes/api/download.js
@@ -6,12 +6,7 @@ const ytdl = require('ytdl-core');
 router.get('/:id/:itag', async (req, res) => {
     try {
         const url = `https://www.youtube.com/watch?v=${req.params.id}`;
-        // res.writeHead(200, {
-        //     'Content-Type': 'text/plain; charset=utf-8',
-        //     'Transfer-Encoding': 'chunked',
-        //     'X-Content-Type-Options': 'nosniff',
-        //     'Access-Control-Allow-Origin': '*'
-        // });
+
         ytdl(url, {
             requestOptions: {
                 'Content-Type': 'text/plain; charset=utf-8',
@@ -22,12 +17,9 @@ router.get('/:id/:itag', async (req, res) => {
             , quality: `${req.params.itag}`, filter: format => format.container === 'mp4'
         }).on('data', (chunk) => {
             res.write(chunk);
-
-            //console.log('chunk', chunk)
         }).on('end', () => {
             res.end();
         })
-        //console.log('YT', yt);
 
     } catch (err) {
         res.json({ fatal: err.message })
