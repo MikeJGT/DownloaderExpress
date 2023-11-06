1ad64bbe35e228aac110e8d8b9631efb9bf84a22
MikeJGT <117930380+MikeJGT@users.noreply.github.com>
2023-11-06 21:19:11 +0100

    Refactor info.model.js

--- models/info.model.js
+++ models/info.model.js
@@ -1,35 +1,6 @@
 const ytdl = require('ytdl-core');
 const router = require('express').Router();
 
-// const audioOrVideo = (tipo, id) => {
-//     const url = `https://www.youtube.com/watch?v=${id}`;
-//     ytdl.getInfo(url).then((result) => {
-//         let formatos = result.formats;
-
-//         let audioVideo = formatos.filter((formato) => {
-//             if (tipo === 'audio') {
-//                 //solo audio
-//                 return (formato.audioCodec !== null) && (formato.videoCodec === null) && formato.container === 'mp4';
-//             }
-
-//             //audio y video
-//             return formato.codecs.split(',').length >= 2;
-//         })
-//         // console.log('Formatos', formatos)
-//         // for (let formato of formatos) {
-//         //     console.log('Quality:', formato.qualityLabel, ' Container:', formato.container, ',Codecs:', formato.codecs, 'Itag: ', formato.itag);
-//         // }
-
-//         //console.log('Resul', audioVideo);
-//         for (let tag of audioVideo) {
-//             // console.log(`Calidad: ${tag.qualityLabel},Codecs: ${tag.codecs},ITAG: ${tag.itag}`)
-//         }
-//         return 'AudioVideo2'
-//         return audioVideo;
-//     })
-// }
-
-
 const audioOrVideo = async (id) => {
     const url = `https://www.youtube.com/watch?v=${id}`;
     let result = await ytdl.getInfo(url);
@@ -44,15 +15,6 @@ const audioOrVideo = async (id) => {
         //audio y video
         return formato.codecs.split(',').length >= 2;
     })
-    // console.log('Formatos', formatos)
-    // for (let formato of formatos) {
-    //     console.log('Quality:', formato.qualityLabel, ' Container:', formato.container, ',Codecs:', formato.codecs, 'Itag: ', formato.itag);
-    // }
-
-    //console.log('Resul', audioVideo);
-    //for (let tag of audioVideo) {
-    // console.log(`Calidad: ${tag.qualityLabel},Codecs: ${tag.codecs},ITAG: ${tag.itag}`)
-    //}
     return [audio, video];
     return 'AudioVideo2'
 }
