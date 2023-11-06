db923509cee72d0140aa0d47102134a49a8f085a
MikeJGT <117930380+MikeJGT@users.noreply.github.com>
2023-11-06 21:18:29 +0100

    Update download.model.js

--- models/download.model.js
+++ models/download.model.js
@@ -4,19 +4,10 @@ const { resolve } = require('path');
 
 const getVideo = (id, res) => {
     const url = `https://www.youtube.com/watch?v=${id}`;
-    // data;
-    // return new Promise((resolve, reject) => {
     let data = ytdl(url, { quality: '22', filter: format => format.container === 'mp4' }).on('data', (chunk) => {
-        //console.log('chunk', chunk)
         res.write(chunk);
     })
-    // .on('end', async () => {
-    //     //console.log('data', data);
-    //     resolve(data.read());
-    // })
 }
-
-
 
 module.exports = {
     getVideo
