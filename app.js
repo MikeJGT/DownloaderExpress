2bd7f05200c928961655ec27e5e72def89be3522
miky3000 <mikecode808@gmail.com>
2023-11-10 20:38:15 +0100

    Fixed Streaming Mime Types

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const ytdl = require('ytdl-core');
 const cors = require('cors');
 
 var app = express();
 
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/api/info.js
+++ routes/api/info.js
@@ -1,15 +1,9 @@
 const router = require('express').Router();
-
 const { audioOrVideo, information } = require("../../models/info.model");
-
 
 router.get('/:id', async (req, res) => {
     try {
         const info = await information(req.params.id);
-        //const info2 = audio2();
-
-        console.log('InfoLog', info.videoDetails)
-        // res.send('HOLA');
         res.send(info.videoDetails);
 
     } catch (err) {
@@ -20,12 +14,7 @@ router.get('/:id', async (req, res) => {
 router.get('/itag/:id', async (req, res) => {
     try {
         const info = await audioOrVideo(req.params.id);
-        //const info2 = audio2();
-
-        console.log('InfoLog', info)
-        // res.send('HOLA');
         res.send(info);
-
     } catch (err) {
         res.json({ fatal: err.message })
     }
